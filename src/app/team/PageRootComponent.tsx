de9ac027211e394d52cce27bc992cecc9403d81c
Abu Taher Muhammad <abut1081@gmail.com>
2023-12-02 15:03:20 +0600

    updated `/team` page & components

--- src/app/team/PageRootComponent.tsx
+++ src/app/team/PageRootComponent.tsx
@@ -1,14 +1,51 @@
+"use client";
+import TeamTable from "@/components/team/TeamTable";
 import DashboardLayout from "@/components/ui/dashboard/DashboardLayout";
-import { memo } from "react";
+import { FC, memo } from "react";
 
+export type TeamItemType = {
+    _id: string;
+    avatar: string;
+    name: string;
+    designation: string;
+    period_from: string;
+    period_to: string;
+    phone: string;
+    email: string;
+    location: string;
+    status: string;
+};
 
-const PageRootComponent = () => {
+export type TeamResponseType = {
+    data: TeamItemType[];
+    offset: number;
+    total: number;
+    limit: number;
+};
+
+export type PageConfig = {
+    analytics?: boolean;
+    settings?: boolean;
+    table?: boolean;
+};
+
+interface PageRootComponentPropsType {
+    data: TeamResponseType;
+    config: PageConfig;
+}
+
+const PageRootComponent: FC<PageRootComponentPropsType> = ({ data, config }: PageRootComponentPropsType) => {
     return (
         <DashboardLayout>
-            <h1>Team</h1>
+            <div className="grid grid-cols-1 gap-22px p-[22px]">
+                <TeamTable
+                    title="Team members"
+                    description={`Showing ${data?.limit} of ${data?.total} members`}
+                    data={data?.data}
+                />
+            </div>
         </DashboardLayout>
     );
 };
-
 
 export default memo(PageRootComponent);
--- src/app/team/page.tsx
+++ src/app/team/page.tsx
@@ -1,12 +1,28 @@
-import PageRootComponent from "./PageRootComponent";
+import PageRootComponent, { PageConfig, TeamResponseType } from "./PageRootComponent";
 import { memo } from "react";
 
+/**
+ * Retrieves data from the specified URL.
+ *
+ * @return {Promise<RequestResponseType>} The retrieved data.
+ */
+async function getData(): Promise<TeamResponseType> {
+    const res = await fetch("http://localhost:3000/data/team.json");
+    const data = await res.json();
+
+    return data;
+}
 
 const PageTeams = async () => {
-    // const data = await getData();
+    const data = await getData();
+    const config: PageConfig = {
+        analytics: false,
+        settings: false,
+        table: true,
+    };
 
     return (
-        <PageRootComponent />
+        <PageRootComponent data={data} config={config} />
     );
 };
 
