a0aed30edaee4bc2aa751aec2e75d5a98a418dc0
Abu Taher Muhamamd <abut1081@gmail.com>
2024-01-18 23:59:29 +0600

    refactor(PWA): `disabled` on `dev` stage

--- next.config.js
+++ next.config.js
@@ -99,6 +99,9 @@ const nextConfig = {
 };
 const withPWA = require("next-pwa")({
   dest: "public",
+  disable: process.env.NODE_ENV === "development",
+  register: true,
+  skipWaiting: true,
 });
 
 const withBundleAnalyzer = require("@next/bundle-analyzer")({
